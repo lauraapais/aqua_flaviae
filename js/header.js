c5fae3a506f6bde5e27d9dcb70e538a34a7cdd86
Laura Pais <pais.laura@gmail.com>
2024-06-05 18:11:01 +0100

    ++

--- js/header.js
+++ js/header.js
@@ -16,37 +16,34 @@ closeMenu.addEventListener("click", function () {
 });
 
 
-    const logoHeader = document.getElementById('logoEstatico');
-    const footer = document.getElementById('footer');
+const logoHeader = document.getElementById('logoEstatico');
+const footer = document.getElementById('footer');
 
-    function checkOverlap() {
-        const logoRect = logoHeader.getBoundingClientRect();
-        const footerRect = footer.getBoundingClientRect();
+function checkOverlap() {
+    const logoRect = logoHeader.getBoundingClientRect();
+    const footerRect = footer.getBoundingClientRect();
 
-        if (logoRect.bottom > footerRect.top && logoRect.top < footerRect.bottom) {
-            logoHeader.classList.add('logoDifferent');
-        } else {
-            logoHeader.classList.remove('logoDifferent');
-        }
+    if (logoRect.bottom > footerRect.top && logoRect.top < footerRect.bottom) {
+        logoHeader.classList.add('logoDifferent');
+    } else {
+        logoHeader.classList.remove('logoDifferent');
     }
+}
 
-    window.addEventListener('scroll', checkOverlap);
-    window.addEventListener('resize', checkOverlap);
-    checkOverlap(); 
+window.addEventListener('scroll', checkOverlap);
+window.addEventListener('resize', checkOverlap);
+checkOverlap();
 
 
 
-    // Seleciona o ícone do menu e o menu mobile
 const menuIconMobile = document.getElementById('menuIconMobile');
 const menuMobile = document.querySelector('.menuMobile');
 const closeMenuMobile = document.getElementById('closeMenuMobile');
 
-// Adiciona o evento de clique para abrir o menu mobile
 menuIconMobile.addEventListener('click', () => {
     menuMobile.classList.add('active');
 });
 
-// Adiciona o evento de clique para fechar o menu mobile
 closeMenuMobile.addEventListener('click', () => {
     menuMobile.classList.remove('active');
 });

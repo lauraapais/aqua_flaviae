add86b5441ac71d28e951d7897e460089de62231
Laura Pais <pais.laura@gmail.com>
2024-06-05 11:02:47 +0100

    ++

--- js/mapa.js
+++ js/mapa.js
@@ -6,7 +6,6 @@ function hideAllInfo() {
 
 document.getElementById('ellipseTermas').addEventListener('click', function () {
     const info = document.getElementById('infoEllipseTermas');
-    // Se a informação estiver visível, ocultá-la
     if (info.style.display === 'block') {
         info.style.display = 'none';
     } else {
@@ -15,7 +14,6 @@ document.getElementById('ellipseTermas').addEventListener('click', function () {
     }
 });
 
-// Adicionar evento de clique para ellipsePonte
 document.getElementById('ellipsePonte').addEventListener('click', function () {
     const info = document.getElementById('infoEllipsePonte');
     if (info.style.display === 'block') {

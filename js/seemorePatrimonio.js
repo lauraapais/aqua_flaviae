eb18a80e2a0f1d320305c15f30c13bba79c8ea1b
Laura Pais <pais.laura@gmail.com>
2024-06-12 15:06:11 +0100

    ++

--- js/seemorePatrimonio.js
+++ js/seemorePatrimonio.js
@@ -50,3 +50,61 @@ seeLess_Ponte.addEventListener("click", function () {
     seeMore_Ponte.style.display = "block";
     seeLess_Ponte.style.display = "none";
 });
+
+
+
+
+
+
+var seeMore_Hidraul = document.getElementById("seeMore_Hidraul");
+var seeLess_Hidraul = document.getElementById("seeLess_Hidraul");
+var hidden_Hidraul = document.getElementById("hidden_Hidraul");
+
+seeMore_Hidraul.addEventListener("click", function () {
+    hidden_Hidraul.classList.add("open");
+    var contentHeight = hidden_Hidraul.scrollHeight + "px";
+    hidden_Hidraul.classList.remove("open");
+
+    hidden_Hidraul.style.maxHeight = contentHeight;
+    hidden_Hidraul.classList.add("open");
+
+    seeMore_Hidraul.style.display = "none";
+    seeLess_Hidraul.style.display = "block";
+});
+
+seeLess_Hidraul.addEventListener("click", function () {
+    hidden_Hidraul.style.maxHeight = "0"; 
+    hidden_Hidraul.classList.remove("open");
+    
+    seeMore_Hidraul.style.display = "block";
+    seeLess_Hidraul.style.display = "none";
+});
+
+
+
+
+
+var seeMore_Escombros = document.getElementById("seeMore_Escombros");
+var seeLess_Escombros = document.getElementById("seeLess_Escombros");
+var hidden_Escombros = document.getElementById("hidden_Escombros");
+
+seeMore_Escombros.addEventListener("click", function () {
+    hidden_Escombros.classList.add("open");
+    var contentHeight = hidden_Escombros.scrollHeight + "px";
+    hidden_Escombros.classList.remove("open");
+
+    hidden_Escombros.style.maxHeight = contentHeight;
+    hidden_Escombros.classList.add("open");
+
+    seeMore_Escombros.style.display = "none";
+    seeLess_Escombros.style.display = "block";
+});
+
+seeLess_Escombros.addEventListener("click", function () {
+    hidden_Escombros.style.maxHeight = "0"; 
+    hidden_Escombros.classList.remove("open");
+    
+    seeMore_Escombros.style.display = "block";
+    seeLess_Escombros.style.display = "none";
+});
+

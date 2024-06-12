3b6b11bec0ff369a62cfe97796861cbb281d59dd
Laura Pais <pais.laura@gmail.com>
2024-06-12 13:10:51 +0100

    ++

--- js/seemorePatrimonio.js
+++ js/seemorePatrimonio.js
@@ -17,7 +17,36 @@ seeMore_Ninfeu.addEventListener("click", function () {
 seeLess_Ninfeu.addEventListener("click", function () {
     hidden_Ninfeu.style.maxHeight = "0"; 
     hidden_Ninfeu.classList.remove("open");
-    
+
     seeMore_Ninfeu.style.display = "block";
     seeLess_Ninfeu.style.display = "none";
+});
+
+
+
+
+
+
+var seeMore_Ponte = document.getElementById("seeMore_Ponte");
+var seeLess_Ponte = document.getElementById("seeLess_Ponte");
+var hidden_Ponte = document.getElementById("hidden_Ponte");
+
+seeMore_Ponte.addEventListener("click", function () {
+    hidden_Ponte.classList.add("open");
+    var contentHeight = hidden_Ponte.scrollHeight + "px";
+    hidden_Ponte.classList.remove("open");
+
+    hidden_Ponte.style.maxHeight = contentHeight;
+    hidden_Ponte.classList.add("open");
+
+    seeMore_Ponte.style.display = "none";
+    seeLess_Ponte.style.display = "block";
+});
+
+seeLess_Ponte.addEventListener("click", function () {
+    hidden_Ponte.style.maxHeight = "0"; 
+    hidden_Ponte.classList.remove("open");
+    
+    seeMore_Ponte.style.display = "block";
+    seeLess_Ponte.style.display = "none";
 });
